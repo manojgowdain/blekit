db1eabc8b0b44ce789782d07f634e25933d6f60c
ManojGowda89 <mail@manojgowda.in>
2026-08-24 16:14:42 +0530

    fix

--- dist/index.mjs
+++ dist/index.mjs
@@ -329,7 +329,7 @@ var SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214";
 var CHARACTERISTICS = {
   data: "19b10001-e8f2-537e-4f6c-d104768a1214",
   reset: "19b10002-e8f2-537e-4f6c-d104768a1214",
-  "time": "19b10003-e8f2-537e-4f6c-d104768a1214"
+  time: "19b10003-e8f2-537e-4f6c-d104768a1214"
 };
 
 // src/KalmanFilter.ts
